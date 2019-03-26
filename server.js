96fbc851294b9b1914cb0875ee9549d6ffad81d4
chenwengao <chenwengao@jd.com>
2019-03-26 17:01:27 +0800

    v

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ try{
 function md5(str) {
   return require('crypto').createHash('md5').update(str).digest('hex')
 }
+function cut(str, a, b) {
+  return str.replace(new RegExp('(^.{'+a+'}).*?(.{'+b+'}$)', 'g'), '$1...$2')
+}
 require('http').createServer((req, res)=>{
   let c=unescape(unescape(req.url).replace(/^.*?\?url=.*?\?url=/, ''))
-  let u=`${c}?chrome-url-focus`, d=`// ${u}?\r\nconsole.log('hooked')\r\n`
-  let f=__dirname+'/data/'+c.replace(/^https*\:\/\/|\?.*/g, '').replace(/[^a-z\d\.]/ig, '_').replace(/\.[^\.]+$/, _=>'.'+md5(u)+_)
+  let u=`${c}#chrome-url-focus`, d=`// ${u}?\r\n// you can paste the formatted code here\r\n`
+  let f=__dirname+'/data/'+c.replace(/^https*\:\/\/(.+?)\/.*?([^\/]+?)(?:\?.*|$)/g, (_, a, b)=>{
+    return (a+'/'+md5(u)+'/'+cut(b, 15, 15)).replace(/[^a-z\d\.]/ig, '_')
+  })
   if(!fs.existsSync(f) || fs.readFileSync(f, 'utf-8')===d) {
     fs.writeFileSync(f, d)
     res.writeHead(302, {'Location': u})
