59ef17069ea1756d6d1561df9bee46a80fd02474
chenwengao <chenwengao@jd.com>
2019-03-26 16:14:02 +0800

    v

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const querystring=require('querystring')
 try{
   fs.mkdirSync(__dirname+'/data')
 }catch(e){}
-
+function md5(str) {
+  return require('crypto').createHash('md5').update(str).digest('hex')
+}
 require('http').createServer((req, res)=>{
   let c=unescape(unescape(req.url).replace(/^.*?\?url=.*?\?url=/, ''))
-  let f=__dirname+'/data/'+c.replace(/^https*\:\/\/|\?.*/g, '').replace(/[^a-z\d\.]/ig, '_')
   let u=`${c}?chrome-url-focus`, d=`// ${u}?\r\nconsole.log('hooked')\r\n`
+  let f=__dirname+'/data/'+c.replace(/^https*\:\/\/|\?.*/g, '').replace(/[^a-z\d\.]/ig, '_').replace(/\.[^\.]+$/, _=>'.'+md5(u)+_)
   if(!fs.existsSync(f) || fs.readFileSync(f, 'utf-8')===d) {
     fs.writeFileSync(f, d)
     res.writeHead(302, {'Location': u})
