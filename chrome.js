8752fedeaea942165c75dc4a5c0e55f246de2dc1
chenwengao <chenwengao@jd.com>
2019-03-27 19:10:47 +0800

    v

--- chrome.js
+++ chrome.js
@@ -1,8 +1,5 @@
-chrome.webRequest.onBeforeRequest.addListener(({url})=>(
-  (
-    url.match(/^https*\:\/\/.*?(\.js|\.css)(\?|$|#)/i) &&
-    !url.match(/chrome-url-focus/)
-  )? {
-    redirectUrl: 'http://127.0.0.1:23456/?url='+escape(url),
-  }: {}
-),{urls: ["<all_urls>"]},["blocking"])
+chrome.webRequest.onBeforeRequest.addListener(function({url, type}) {
+  let hook=url.match(/^https*\:\/\/.*?(\.js|\.css)(\?|$|#)/i) && !url.match(/chrome-url-focus|23456/)
+  if(!hook) return {}
+  return {redirectUrl: 'http://127.0.0.1:23456/?url='+escape(url)}
+}, {urls: ["<all_urls>"]},["blocking"])
